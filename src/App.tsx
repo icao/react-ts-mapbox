1b3e4bc29501762eedc3cd7cc3bf06a99cd126cf
César Aparicio <clark012389@hotmail.com>
2022-03-16 21:29:32 -0600

    Project base

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="w-full h-screen bg-slate-50">
+        <h1 className="text-slate-900 text-5xl font-uber font-medium">
+          How much does a ride with Uber cost?
+        </h1>
+        {/* <div className="p-4 bg-white rounded-md w-3/5 h-auto shadow-black-large">
+          <p className="text-slate-900 font-nomal text-2xl font-normal font-uberText">
+            Calculador de tarifas de Uber
+          </p>
+        </div> */}
+        <br />
+        <br />
+        {/* <button className="bg-next-blue m-0 px-14 py-1 leading-9 rounded-md shadow-blue-next text-white capitalize text-center font-uberText text-base">
+          enviar petición
+        </button> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
