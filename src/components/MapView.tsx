7f0bdb186513afdb7a9ca285b842151859175de0
César Aparicio <clark012389@hotmail.com>
2022-03-22 15:28:07 -0600

    Adding Map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,18 +1,28 @@
-import { useContext } from "react";
+import { Map } from "mapbox-gl";
+import { useContext, useLayoutEffect, useRef } from "react";
 import { PlacesContext } from "../context";
 import { Loading } from "./Loading";
 
 export const MapView = () => {
   const { isLoading, userLocation } = useContext(PlacesContext);
+  const mapContainer = useRef<HTMLDivElement>(null);
 
-  // if (isLoading) {
-  //   return <Loading />;
-  // }
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new Map({
+        container: mapContainer.current!, // container ID
+        style: "mapbox://styles/mapbox/dark-v10", // style URL
+        // style: "mapbox://styles/mapbox/light-v10", // style URL
+        center: userLocation, // starting position [lng, lat]
+        zoom: 14, // starting zoom
+      });
+    }
+  }, [isLoading]);
 
   return (
-    <>
+    <div ref={mapContainer} className="bg-zinc-800 w-full h-screen">
       {isLoading && <Loading />}
       <p>{userLocation?.join(",")}</p>
-    </>
+    </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import MapsApp from "./MapsApp";
 
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaWNhb3MiLCJhIjoiY2wxMW5pMGplMndmZDNjcW9sNWkydnYzYyJ9.O5VDvBOQ2bLM9gQZ0UAzlQ";
+
+if (!navigator.geolocation) {
+  alert("Lo sentimos, tu navegador no tiene soporte para Geolocalización");
+  throw new Error(
+    "Lo sentimos, tu navegador no tiene soporte para Geolocalización"
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <MapsApp />
