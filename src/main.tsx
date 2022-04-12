d7040a595f1170ff11a3c574c175558a39b97c65
César Aparicio <clark012389@hotmail.com>
2022-04-12 18:33:52 -0500

    Updated projecto to React 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import MapsApp from './MapsApp'
-
+import { createRoot } from 'react-dom/client'
 import mapboxgl from 'mapbox-gl'
+import MapsApp from './MapsApp'
+import './index.css'
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiaWNhb3MiLCJhIjoiY2wxMW5pMGplMndmZDNjcW9sNWkydnYzYyJ9.O5VDvBOQ2bLM9gQZ0UAzlQ'
@@ -13,9 +12,11 @@ if (!navigator.geolocation) {
   throw new Error('Lo sentimos, tu navegador no tiene soporte para Geolocalización')
 }
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <MapsApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
